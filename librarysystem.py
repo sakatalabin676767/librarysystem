f33e63a9b348647c1c3d24489af4ab8389e9fa85
zoravis <73932194+zoravis@users.noreply.github.com>
2026-04-28 04:45:56 +0000

    update

--- librarysystem.py
+++ librarysystem.py
@@ -17,11 +17,23 @@ def showbooks(): # student C
     with open("library.txt", "r") as f:
         books = f.read()
         print(books)
+
+def updatebooks():
+    with open("library.txt", 'r') as f:
+        content = f.read()
+        old_text = input("\nEnter book to update: ")
+    new_text = input("Enter updated book: ")
+    new_content = content.replace(old_text, new_text)
+    with open("library.txt", 'w') as f:
+        f.write(new_content)
+    print("Book Updated Successfully!\n")
+
         
 while True:
     print("1. Add Book") # student B
     print("2. Show Books") # student C 
-    print("3. Exit")
+    print("3. Update Book")
+    print("4. Exit")
     try:
         choice = int(input("Enter choice: "))
     except ValueError:
@@ -32,6 +44,8 @@ while True:
     elif choice == 2:
         showbooks()
     elif choice == 3:
+        updatebooks()
+    elif choice == 4:
         print("Thank you for using the program!")
         break
     else:
