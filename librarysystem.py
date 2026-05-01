feb8a93c9fa89fdb81e1c5b3a9d016c5941108bf
snomathy <snomrelay@gmail.com>
2026-05-01 05:29:03 +0000

    added delete functionality

--- librarysystem.py
+++ librarysystem.py
@@ -7,7 +7,6 @@ except FileExistsError:
     print("File already exists!")
 
 def addbook():
-    print("---Add Book---")
     title = input("Enter book title: ")
     with open("library.txt", "a") as f:
         f.write(title + "\n")
@@ -22,40 +21,59 @@ def updatebooks():
     with open("library.txt", 'r') as f:
         content = f.read()
         old_text = input("\nEnter book to update: ")
-    new_text = input("Enter updated book: ")
-    new_content = content.replace(old_text, new_text)
-    with open("library.txt", 'w') as f:
-        f.write(new_content)
-    print("Book Updated Successfully!\n")
+    if old_text not in content:
+        print("Book not found!")
+    else:
+        new_text = input("Enter updated book: ")
+        new_content = content.replace(old_text, new_text).strip()
+        with open("library.txt", 'w') as f:
+            f.write(new_content)
+        print("Book Updated Successfully!\n")
 
 def searchbook():
     with open("library.txt", 'r') as f:
         content = f.read()
     textfind = input("Search for book: ")
     if textfind in content:
-        print("Book found!)
+        print("Book found!")
+    else:
+        print("Book not found!")
+
+def deletebook():
+    with open("library.txt", 'r') as f:
+        content = f.read()
+    bookdelete = input("Input book to delete: ")
+    if bookdelete in content:
+        newstuff = content.replace(bookdelete, "").strip()
+        with open("library.txt", 'w') as f:
+            f.write(newstuff)
+        print("Book deleted successfully!\n")
+    else:
+        print("Book not found!")
 
 while True:
     print("1. Add Book")
     print("2. Show Books")
     print("3. Update Book")
     print("4. Search Book")
-    print("5. Exit")
+    print("5. Delete Book")
+    print("6. Exit")
     try:
         choice = int(input("Enter choice: "))
+        if choice == 1:
+            addbook()
+        elif choice == 2:
+            showbooks()
+        elif choice == 3:
+            updatebooks()
+        elif choice == 4:
+            searchbook()
+        elif choice == 5:
+            deletebook()
+        elif choice == 6:
+            print("Thank you for using the program!")
+            break
+        else:
+            print("Invalid choice!")
     except ValueError:
         print("Invalid input!")
-
-    if choice == 1:
-        addbook()
-    elif choice == 2:
-        showbooks()
-    elif choice == 3:
-        updatebooks()
-    elif choice == 4:
-        searchbook()
-    elif choice == 5:
-        print("Thank you for using the program!")
-        break
-    else:
-        print("Invalid choice!")
